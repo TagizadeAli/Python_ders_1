d674654498baad7674351da850fd3eb34321280c
TagizadeAli <42002414+TagizadeAli@users.noreply.github.com>
2020-10-31 14:14:59 +0400

    Create ders.py

---
+++ ders.py
@@ -0,0 +1,32 @@
+# int , float , string.
+
+tam = 5
+
+kesr = 5.5
+
+text = "salam"
+
+# 'salam' " resad'in " """
+# 
+#   salam resad
+#   salam abbas
+# 
+# 
+# 
+# """
+tek = 'salam'
+cut = "salam"
+uc = """
+
+salam resad
+salam abbas
+
+"""
+
+
+print(tam)
+print(kesr)
+print(text)
+print(tek)
+print(cut)
+print(uc)
